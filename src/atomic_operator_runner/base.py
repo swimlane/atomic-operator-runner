9b34cdab409893f44ea2475a24da25b31071a346
MSAdministrator <rickardja@live.com>
2022-08-15 15:10:05 -0500

    fix: Updated formatting of base class

--- src/atomic_operator_runner/base.py
+++ src/atomic_operator_runner/base.py
@@ -4,25 +4,17 @@ from .utils.logger import LoggingBase
 
 
 class Base(metaclass=LoggingBase):
-    
+
     COMMAND_MAP = {
-        'command_prompt': {
-            'windows': 'C:\\Windows\\System32\\cmd.exe',
-            'linux': '/bin/sh',
-            'macos': '/bin/sh',
-            'default': '/bin/sh'
+        "command_prompt": {
+            "windows": "C:\\Windows\\System32\\cmd.exe",
+            "linux": "/bin/sh",
+            "macos": "/bin/sh",
+            "default": "/bin/sh",
         },
-        'powershell': {
-            'windows': 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
-        },
-        'sh': {
-            'linux': '/bin/sh',
-            'macos': '/bin/sh'
-        },
-        'bash': {
-            'linux': '/bin/bash',
-            'macos': '/bin/bash'
-        }
+        "powershell": {"windows": "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"},
+        "sh": {"linux": "/bin/sh", "macos": "/bin/sh"},
+        "bash": {"linux": "/bin/bash", "macos": "/bin/bash"},
     }
     ELEVATION_COMMAND_MAP = {
         "powershell": "Start-Process PowerShell -Verb RunAs;",
@@ -30,9 +22,10 @@ class Base(metaclass=LoggingBase):
         "command_prompt": "cmd.exe /c",
         "sh": "sudo",
         "bash": "sudo",
-        "ssh": "sudo"
+        "ssh": "sudo",
     }
 
+    # properties used throughout the project
     hostname = None
     username = None
     password = None
@@ -55,11 +48,16 @@ class Base(metaclass=LoggingBase):
         """
         if data:
             # Remove Windows CLI garbage
-            data = re.sub(r"Microsoft\ Windows\ \[version .+\]\r?\nCopyright.*(\r?\n)+[A-Z]\:.+?\>", "", data.decode("utf-8", "ignore"))
+            data = re.sub(
+                r"Microsoft\ Windows\ \[version .+\]\r?\nCopyright.*(\r?\n)+[A-Z]\:.+?\>",
+                "",
+                data.decode("utf-8", "ignore"),
+            )
             # formats strings with newline and return characters
             return re.sub(r"(\r?\n)*[A-Z]\:.+?\>", "", data)
+        return data
 
-    def print_process_output(self, command, return_code, output, errors):
+    def print_process_output(self, command: str, return_code: int, output, errors):
         """Outputs the appropriate outputs if they exists to the console and log files
 
         Args:
@@ -70,16 +68,20 @@ class Base(metaclass=LoggingBase):
         """
         return_dict = {}
         if return_code == 127:
-            return_dict['error'] =  f"\n\nCommand Not Found: {command} returned exit code {return_code}: \nErrors: {self.clean_output(errors)}/nOutput: {output}"
-            self.__logger.warning(return_dict['error'])
+            return_dict[
+                "error"
+            ] = f"\n\nCommand Not Found: {command} returned exit code {return_code}: \nErrors: {self.clean_output(errors)}/nOutput: {output}"
+            self.__logger.warning(return_dict["error"])
             return return_dict
         if output or errors:
             if output:
-                return_dict['output'] = self.clean_output(output)
-                self.__logger.info("\n\nOutput: {}".format(return_dict['output']))
+                return_dict["output"] = self.clean_output(output)
+                self.__logger.info("\n\nOutput: {}".format(return_dict["output"]))
             else:
-                return_dict['error'] =  f"\n\nCommand: {command} returned exit code {return_code}: \n{self.clean_output(errors)}"
-                self.__logger.warning(return_dict['error'])
+                return_dict[
+                    "error"
+                ] = f"\n\nCommand: {command} returned exit code {return_code}: \n{self.clean_output(errors)}"
+                self.__logger.warning(return_dict["error"])
         else:
             self.__logger.info("(No output)")
         return return_dict
