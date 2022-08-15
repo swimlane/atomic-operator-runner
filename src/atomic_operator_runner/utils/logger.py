6a6e699b680a98c6ae2af64f2bac2910c916d99c
MSAdministrator <rickardja@live.com>
2022-08-15 15:11:34 -0500

    fix(logger): Updated logger to latest version

--- src/atomic_operator_runner/utils/logger.py
+++ src/atomic_operator_runner/utils/logger.py
@@ -1,13 +1,42 @@
-import os
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, Swimlane <info@swimlane.com>
+# MIT License (see LICENSE or https://opensource.org/licenses/MIT)
 import logging.config
+import os
+from logging import DEBUG, FileHandler, Formatter
+
 import yaml
 
-from logging import FileHandler, DEBUG, INFO, ERROR, WARNING, CRITICAL
-import logging
+
+class CustomFormatter(Formatter):
+    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
+
+    grey = "\x1b[38;21m"
+    blue = "\x1b[38;5;39m"
+    yellow = "\x1b[38;5;226m"
+    red = "\x1b[38;5;196m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.grey + self.fmt + self.reset,
+            logging.INFO: self.blue + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.bold_red + self.fmt + self.reset,
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 
 class DebugFileHandler(FileHandler):
-    def __init__(self, filename, mode='a', encoding=None, delay=False):
+    def __init__(self, filename, mode="a", encoding=None, delay=False):
         super().__init__(filename, mode, encoding, delay)
 
     def emit(self, record):
@@ -22,22 +51,26 @@ class LoggingBase(type):
         cls.setup_logging()
 
         # Explicit name mangling
-        logger_attribute_name = '_' + cls.__name__ + '__logger'
+        logger_attribute_name = "_" + cls.__name__ + "__logger"
 
         # Logger name derived accounting for inheritance for the bonus marks
-        logger_name = '.'.join([c.__name__ for c in cls.mro()[-2::-1]])
+        logger_name = ".".join([c.__name__ for c in cls.mro()[-2::-1]])
 
         setattr(cls, logger_attribute_name, logging.getLogger(logger_name))
 
-    def setup_logging(cls, default_path='./src/atomic_operator_runner/data/logging.yml', default_level=logging.INFO, env_key='LOG_CFG'):
-        """Setup logging configuration
-        """
+    def setup_logging(
+        cls,
+        default_path="./aqueduct/data/logging.yml",
+        default_level=logging.INFO,
+        env_key="LOG_CFG",
+    ):
+        """Setup logging configuration"""
         path = os.path.abspath(os.path.expanduser(os.path.expandvars(default_path)))
         value = os.getenv(env_key, None)
         if value:
             path = value
         if os.path.exists(os.path.abspath(path)):
-            with open(path, 'rt') as f:
+            with open(path, "rt") as f:
                 config = yaml.safe_load(f.read())
             logger = logging.config.dictConfig(config)
         else:
